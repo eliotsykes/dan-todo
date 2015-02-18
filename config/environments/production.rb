aded58fd1d52b8e4931dae8bec707ed9f72ddb58
dannyvassallo <danielvassallo87@gmail.com>
2015-02-18 00:13:21 -0500

    removed unnecessary lines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'dan-blocitoff.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
 end
