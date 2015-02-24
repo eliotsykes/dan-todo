fabe4bc33f4d5866b451b05daa57eb7a896ae8e2
Dan Vassallo <danielvassallo87@gmail.com>
2015-02-24 15:53:58 -0500

    changed index/show

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,6 +2,10 @@ class ListsController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @list = List.find(params[:id])
+  end
+
+  def index
     @lists = current_user.lists
   end
 
