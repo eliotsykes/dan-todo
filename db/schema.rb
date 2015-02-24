a1e3204a8f12ea8e5954482adaf1a63bac7c616b
Dan Vassallo <danielvassallo87@gmail.com>
2015-02-24 17:45:52 -0500

    added/installed pundit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150223023212) do
+ActiveRecord::Schema.define(version: 20150224222828) do
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150223023212) do
     t.string   "unconfirmed_email",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
