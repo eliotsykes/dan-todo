1b196ae5f9456c02f937a68376010c8798ed1e6e
dannyvassallo <danielvassallo87@gmail.com>
2015-03-02 22:09:25 -0500

    generated Item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224222828) do
+ActiveRecord::Schema.define(version: 20150303030742) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "lists", force: :cascade do |t|
     t.string   "title"
