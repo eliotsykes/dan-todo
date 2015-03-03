9ac2efeb8f7c3aa0155e3b27efd06a4c82efbac3
dannyvassallo <danielvassallo87@gmail.com>
2015-03-02 22:06:14 -0500

    changed @user to current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email)
+    @user = current_user
   end
 
 end
