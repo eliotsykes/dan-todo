e25eb40bfe73d0fc579e118414b6d397d0c3e7f7
dannyvassallo <danielvassallo87@gmail.com>
2015-04-14 11:24:03 -0400

    added unique to api_key through migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328224720) do
-
-  create_table "events", force: :cascade do |t|
-    t.string   "name"
-    t.time     "occurred_at"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 20150414152237) do
 
   create_table "items", force: :cascade do |t|
     t.text     "name"
@@ -60,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150328224720) do
     t.string   "api_key"
   end
 
+  add_index "users", ["api_key"], name: "index_users_on_api_key", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
