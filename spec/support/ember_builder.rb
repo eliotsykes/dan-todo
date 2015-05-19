f9b1d0167fabdcf8720dd92ef1261df8c34df5a1
Eliot Sykes <eliotsykes@gmail.com>
2015-05-19 21:01:39 +0100

    Beginnings of EmberBuilder in spec/support

---
+++ spec/support/ember_builder.rb
@@ -0,0 +1,34 @@
+class EmberBuilder
+
+  def initialize
+    @build_count = 0
+  end
+
+  def build_once
+    build if @build_count == 0
+  end
+
+  def build
+    p "TODO: bin/ember build --environment=test"
+    p "...and wait until it completes (print time output? --quiet option?)"
+    @build_count += 1
+  end
+
+end
+
+ember_builder = EmberBuilder.new
+
+RSpec.configure do |config|
+
+  config.before(:each, type: :feature) do
+    # Assume JavaScript required if the capybara driver is JS-capable. All drivers
+    # except :rack_test are assumed to be JS-capable. Saves time by not running
+    # Ember build when selected feature specs that don't require JS are run.
+    #
+    # Testing current_driver is more dependable than testing for js:true as 
+    # Capybara allows driver to be set with `driver: :selenium` meta data.
+    js_required = Capybara.current_driver != :rack_test
+    ember_builder.build_once if js_required
+  end
+
+end
