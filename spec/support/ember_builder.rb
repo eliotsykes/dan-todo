3db2ae6d8b831776194a01b826769423532b46da
Eliot Sykes <eliotsykes@gmail.com>
2015-05-23 09:40:04 +0100

    Run Ember build before any feature spec
    Non-JS feature specs may still be dependent on files produced
    by the Ember build, so don't limit a fresh build to JS-dependent
    specs only.

--- spec/support/ember_builder.rb
+++ spec/support/ember_builder.rb
@@ -26,14 +26,7 @@ end
 RSpec.configure do |config|
 
   config.before(:each, type: :feature) do
-    # Assume JavaScript required if the capybara driver is JS-capable. All drivers
-    # except :rack_test are assumed to be JS-capable. Saves time by not running
-    # Ember build when selected feature specs that don't require JS are run.
-    #
-    # Testing current_driver is more dependable than testing for js:true as 
-    # Capybara allows driver to be set with `driver: :selenium` meta data.
-    js_required = Capybara.current_driver != :rack_test
-    EmberBuilder.build_once if js_required
+    EmberBuilder.build_once
   end
 
 end
