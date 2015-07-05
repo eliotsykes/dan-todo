02d17841042ab17efc6013416a16536ebcbe0d04
Eliot Sykes <eliotsykes@gmail.com>
2015-07-05 14:35:02 +0100

    Add json_request_headers helper method

--- spec/support/json_helper.rb
+++ spec/support/json_helper.rb
@@ -2,6 +2,10 @@ module JsonHelper
   def json
     @json ||= JSON.parse(response.body, symbolize_names: true)
   end
+
+  def json_request_headers
+    { "Content-Type" => "application/json", "Accept" => "application/json" }
+  end
 end
 
 RSpec.configure do |config|
