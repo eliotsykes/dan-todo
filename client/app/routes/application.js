0b7f863f2a51cf7d7269fa92bef6e7584ff8e102
Eliot Sykes <eliotsykes@gmail.com>
2015-07-25 19:37:57 +0100

    Remove unused comment

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 import ConfigurableAuthenticationRouteMixin from '../mixins/configurable-authentication-route-mixin';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-// Order of beforeModels may not be right any longer - can authenticated routes
-// redirect to session.new still?
 Ember.Route.reopen(ConfigurableAuthenticationRouteMixin);
 
 export default Ember.Route.extend(ApplicationRouteMixin);
