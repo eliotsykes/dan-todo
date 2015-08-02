6a137bf8f5699c5d4e86747b74873f44b3dd8ec9
Eliot Sykes <eliotsykes@gmail.com>
2015-08-02 13:48:51 +0100

    Remove superfluous test

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -13,8 +13,7 @@ feature 'User login', type: :feature, js: true do
 
     visit root_path
     click_link 'Sign in'
-
-    expect(page).to be_sign_in_page
+    expect(page).to be_login_page
 
     fill_in 'Enter your email', with: 'someone@somewhere.example'
     fill_in 'Enter your password', with: 'test password'
@@ -29,9 +28,7 @@ feature 'User login', type: :feature, js: true do
   scenario 'fails with wrong password' do
     create :user, email: 'someone@somewhere.example', password: 'test password', password_confirmation: 'test password'
 
-    visit login_path
-
-    expect(page).to be_sign_in_page
+    visit_login_page
 
     fill_in 'Enter your email', with: 'someone@somewhere.example'
     fill_in 'Enter your password', with: 'wrong password'
@@ -40,15 +37,13 @@ feature 'User login', type: :feature, js: true do
 
     expect(page).to have_text 'Sorry, we failed to sign you in, please try again.'
     expect(page).not_to have_login_success_message
-    expect(page).to be_sign_in_page
+    expect(page).to be_login_page
   end
 
   scenario 'fails with wrong username' do
     create :user, email: 'someone@somewhere.example', password: 'test password', password_confirmation: 'test password'
 
-    visit login_path
-
-    expect(page).to be_sign_in_page
+    visit_login_page
 
     fill_in 'Enter your email', with: 'not-someone@somewhere.example'
     fill_in 'Enter your password', with: 'test password'
@@ -57,12 +52,15 @@ feature 'User login', type: :feature, js: true do
 
     expect(page).to have_text 'Sorry, we failed to sign you in, please try again.'
     expect(page).not_to have_login_success_message
-    expect(page).to be_sign_in_page
+    expect(page).to be_login_page
   end
 
-  xscenario 'prompts for missing credentials'
+  def visit_login_page
+    visit login_path
+    expect(page).to be_login_page
+  end
 
-  def be_sign_in_page
+  def be_login_page
     have_title('Sign In').and have_css(:h1, text: 'Sign In')
   end
 
