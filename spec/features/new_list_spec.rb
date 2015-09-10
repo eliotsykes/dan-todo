b88bb42ed48c6354b6c7535e9510a919b9776a7c
Eliot Sykes <eliotsykes@gmail.com>
2015-09-10 14:14:16 +0100

    Beginning New List Feature Spec

---
+++ spec/features/new_list_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'New list' do
+
+  scenario 'added successfully' do
+    #create user
+
+    #login
+
+    #click new list link
+
+    #enter list title
+    flunk
+  end
+
+  xscenario 'requires title' do
+    # show error message
+  end
+
+  xscenario 'not added when user cancels' do
+    # click the '< Lists' back button after filling in a title
+
+    # Check the list is not shown in index
+  end
+
+
+
+end
