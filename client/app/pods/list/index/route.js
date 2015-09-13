a5611a60e1330522d6dd3155bbd96a2135e5bf9c
Eliot Sykes <eliotsykes@gmail.com>
2015-09-13 13:26:30 +0100

    Introduce editsLocked queryParam

--- client/app/pods/list/index/route.js
+++ client/app/pods/list/index/route.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    editsLocked: {
+      replace: true
+    }
+  },
+
   model() {
     return this.store.findAll('list');
   }
