02b8e4e02dfea941c5eabc43cd3f78b5476ce55b
Eliot Sykes <eliotsykes@gmail.com>
2015-09-18 18:05:30 +0100

    Move edit unlock test to more appropriate feature spec

--- spec/features/edit_list_spec.rb
+++ spec/features/edit_list_spec.rb
@@ -25,4 +25,36 @@ feature 'Edit list', type: :feature, js: true do
     expect(list_titles).to eq ["Food"]
   end
 
+  scenario "unlocking edits provides edit list & done options" do
+    user = create(:user)
+    create(:list, user: user, title: "Cleaning Supplies")
+    create(:list, user: user, title: "Groceries")
+
+    visit_login_page_and_login user: user
+
+    expect(page).to be_lists_page
+    expect(page).to have_link "+ New List"
+    expect(page).to have_no_link "Done"
+
+    within "header" do
+      click_link "Edit"
+      expect(page).to have_no_link "Edit"
+      expect(page).to have_link "Done"
+    end
+
+    expect(page).to have_no_link "+ New List"
+
+    expect(page).to have_css "li[data-list] a", text: "Edit List", count: 2
+
+    within "header" do
+      click_link "Done"
+      expect(page).to have_no_link "Done"
+      expect(page).to have_link "Edit"
+    end
+
+    expect(page).to have_no_link "Edit List"
+    expect(page).to have_link "+ New List"
+  end
+
+
 end
--- spec/features/list_index_spec.rb
+++ spec/features/list_index_spec.rb
@@ -15,29 +15,4 @@ feature 'List index', type: :feature, js: true do
     expect(page).to have_css "li[data-list]", text: "Groceries"
   end
 
-  scenario "edit mode provides edit & cancel options" do
-    user = create(:user)
-    create(:list, user: user, title: "Cleaning Supplies")
-    create(:list, user: user, title: "Groceries")
-
-    visit_login_page_and_login user: user
-
-    expect(page).to be_lists_page
-    expect(page).to have_link "+ New List"
-    expect(page).to have_no_link "Cancel"
-
-    click_link "Edit"
-
-    expect(page).to have_no_link "Edit"
-    expect(page).to have_no_link "+ New List"
-
-    expect(page).to have_css "li[data-list] a", text: "Edit List", count: 2
-
-    click_link "Cancel"
-
-    expect(page).to have_no_link "Edit List"
-    expect(page).to have_link "+ New List"
-    expect(page).to have_no_link "Cancel"
-  end
-
 end
