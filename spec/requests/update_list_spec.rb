a6278ccba68cb4132bed1c41facddf68d7542f85
Eliot Sykes <eliotsykes@gmail.com>
2015-09-18 20:52:11 +0100

    Update list request spec (failing)

--- spec/requests/update_list_spec.rb
+++ spec/requests/update_list_spec.rb
@@ -2,15 +2,32 @@ require 'rails_helper'
 
 describe 'update lists api' do
   it 'updates a list' do
-
     user = create(:user)
-    api_key = user.api_key
-    list1 = create(:list, :title => 'List 1', :user => user)
+    list1 = create(:list, title: 'List 1', user: user)
 
-    put "/api/v1/lists/#{list1.id}.json", {"list" => {"title" => "New Title"}}, "X-Api-Key" => api_key
+    parameters = {list: {title: "New Title"}}.to_json
+    headers = {"X-Api-Key" => user.api_key}.merge(json_request_headers)
 
-    expect(response.status).to eq(204)
-    expect(List.last.title).to eq("New Title")
-    expect(List.last.user).to eq(user)
+    put "/api/v1/lists/#{list1.id}", parameters, headers
+
+    list = List.last
+
+    expect(response).to have_http_status :no_content
+    expect(list.title).to eq("New Title")
+  end
+
+  it 'does not perform an invalid update' do
+    user = create(:user)
+    list = create(:list, title: 'Groceries', user: user)
+
+    parameters = {list: {title: ""}}.to_json
+    headers = {"X-Api-Key" => user.api_key}.merge(json_request_headers)
+
+    put "/api/v1/lists/#{list.id}", parameters, headers
+
+    expect(response).to have_http_status :unprocessable_entity
+    expect(response.content_type).to eq("application/json")
+    expect(json).to eq errors: ["Title can't be blank"]
+    expect(List.last.title).to eq("Groceries")
   end
 end
