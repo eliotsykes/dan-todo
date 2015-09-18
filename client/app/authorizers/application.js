61d48361bd6d9920d186974ddaabd99f7730336a
Eliot Sykes <eliotsykes@gmail.com>
2015-09-18 18:06:15 +0100

    Fix lint errors

--- client/app/authorizers/application.js
+++ client/app/authorizers/application.js
@@ -1,7 +1,8 @@
+import Ember from 'ember';
 import Base from 'simple-auth/authorizers/base';
 
 export default Base.extend({
-  authorize(jqXHR, requestOptions) {
+  authorize(jqXHR/*, requestOptions*/) {
     let isAuthenticated = this.get('session.isAuthenticated');
     let token = this.get('session.secure.token');
 
