b4e5f4b62bc71d645f3daa27f1837de1a40a6991
Eliot Sykes <eliotsykes@gmail.com>
2015-09-19 11:02:52 +0100

    Common transitionToListIndex() function

--- client/app/pods/components/list-form/component.js
+++ client/app/pods/components/list-form/component.js
@@ -4,24 +4,18 @@ export default Ember.Component.extend({
   actions: {
     delete() {
       let cancelled = !window.confirm("Are you sure you want to delete this list?");
+
       if (cancelled) {
         return;
       }
 
       let list = this.get('list');
-
-      let transitionToListIndex = () => {
-        this.get('router').transitionTo(
-          'list.index', { queryParams: { editsLocked: true } }
-        );
-      };
-
       let notifier = this.get('notifier');
       let successMessage = "List deleted.";
 
-      function onSuccess() {
+      let onSuccess = () => {
         notifier.setMessage(successMessage);
-        transitionToListIndex();
+        this.transitionToListIndex();
       }
 
       list.destroyRecord().then(onSuccess);
@@ -29,19 +23,10 @@ export default Ember.Component.extend({
     // save() is called when form is submitted
     save() {
       let list = this.get('list');
-
-      // Use ES6 arrow function => syntax to avoid having to call .bind(this)
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
-      let transitionToListIndex = () => {
-        this.get('router').transitionTo(
-          'list.index', { queryParams: { editsLocked: true } }
-        );
-      };
-
       let hasUnsavedChanges = list.get('isDirty');
 
       if (!hasUnsavedChanges) {
-        transitionToListIndex();
+        this.transitionToListIndex();
         return;
       }
 
@@ -50,12 +35,12 @@ export default Ember.Component.extend({
       let successMessage = this.get('successMessage');
       let errorMessage = this.get('errorMessage');
 
-      function onSuccess() {
+      let onSuccess = () => {
         notifier.setMessage(successMessage);
-        transitionToListIndex();
+        this.transitionToListIndex();
       }
 
-      function onFailure(error) {
+      let onFailure = (error) => {
         let errors = error.responseJSON.errors;
         if (Ember.isPresent(errors)) {
           errorMessage += " " + errors.join(". ");
@@ -66,6 +51,11 @@ export default Ember.Component.extend({
       // Register/save the list via an AJAX request to the server API:
       list.save().then(onSuccess).catch(onFailure);
     }
+  },
+  transitionToListIndex() {
+    this.get('router').transitionTo(
+      'list.index', { queryParams: { editsLocked: true } }
+    );
   }
 
 });
